ca45dc93bac8dcceda08e100ddea7b0e36fb06c0
Fang_Woei <fangwoei22f@forwardschool.edu.my>
2023-01-16 19:26:48 +0800

    cs12simplecms

---
+++ includes/class-post.php
@@ -0,0 +1,102 @@
+<?php
+
+class Post
+{
+     /**
+     * Retrieve all posts from database
+     */
+    public static function getAllPosts( $user_id )
+    {
+        // if is a normal user
+        if ( Authentication::isUser() ) {
+            return DB::connect()->select(
+                'SELECT * FROM posts WHERE user_id = :user_id ORDER BY id DESC',
+                [
+                    'user_id' => $user_id
+                ],
+                true
+            );
+        }
+        return DB::connect()->select(
+            'SELECT * FROM posts ORDER BY id DESC',
+            [],
+            true
+        );
+    }
+
+    /**
+     * Retrieve post data by id
+     */
+    public static function getPostByID( $post_id )
+    {
+        return DB::connect()->select(
+            'SELECT * FROM posts WHERE id = :id',
+            [
+                'id' => $post_id
+            ]
+        );
+    }
+    
+    /**
+     * Retrieve all the publish posts
+     */
+    public static function getPublishPosts()
+    {
+        return DB::connect()->select(
+            'SELECT * FROM posts WHERE status = :status ORDER BY id DESC',
+            [
+                'status' => 'publish'
+            ],
+            true
+        );
+    }
+
+    /**
+     * Add new post
+     */
+    public static function add( $title, $content, $user_id )
+    {
+        return DB::connect()->insert(
+            'INSERT INTO posts (title , content, user_id) 
+            VALUES (:title, :content, :user_id)',
+            [
+                'title' => $title,
+                'content' => $content,
+                'user_id' => $user_id
+            ]
+        );
+    }
+
+    /**
+     * Update Post details
+     */
+    public static function update( $id, $title, $content, $status )
+    {
+
+        // update user data into the database
+        return DB::connect()->update(
+            'UPDATE posts SET 
+            title = :title, content = :content, status = :status 
+            WHERE id = :id',
+            [
+                'id' => $id,
+                'title' => $title,
+                'content' => $content,
+                'status' => $status
+            ]
+        );
+    }
+
+    /**
+     * Delete post
+     */
+    public static function delete( $post_id )
+    {
+        return DB::connect()->delete(
+            'DELETE FROM posts where id = :id',
+            [
+                'id' => $post_id
+            ]
+        );
+    }
+}
---
+++ pages/manage-posts-add.php
@@ -0,0 +1,80 @@
+<?php
+
+    // redirect to login page if not logged in
+    if ( !Authentication::whoCanAccess('user') ) {
+        header( 'Location: /login' );
+        exit;
+    }
+
+
+  // step 1: set CSRF token
+  CSRF::generateToken( 'add_post_form' );
+
+  // step 2: make sure post request
+  if ( $_SERVER["REQUEST_METHOD"] === 'POST' ) {
+
+    // step 3: do error check
+     $rules = [
+      'title' => 'required',
+      'content' => 'required',
+      'csrf_token' => 'add_post_form_csrf_token',
+    ];
+
+    $error = FormValidation::validate(
+      $_POST,
+      $rules
+    );
+
+    // make sure there is no error
+    if ( !$error ) {
+
+      // step 4 = add new user
+      Post::add(
+        $_POST['title'],
+        $_POST['content'],
+        $_SESSION['user']['id']
+      );
+
+      // step 5: remove the CSRF token
+      CSRF::removeToken( 'add_post_form' );
+
+      // step 6: redirect to manage posts page
+      header("Location: /manage-posts");
+      exit;
+
+    } // end - $error
+
+
+  } // end - $_SERVER["REQUEST_METHOD"]
+
+    require dirname(__DIR__) . '/parts/header.php';
+?>
+<div class="container mx-auto my-5" style="max-width: 700px;">
+    <div class="d-flex justify-content-between align-items-center mb-2">
+        <h1 class="h1">Add New Post</h1>
+    </div>
+    <div class="card mb-2 p-4">
+        <form
+            method="POST"
+            action="<?php echo $_SERVER["REQUEST_URI"]; ?>">
+            <div class="mb-3">
+                <label for="post-title" class="form-label">Title</label>
+                <input type="text" class="form-control" id="post-title" name="title">
+            </div>
+            <div class="mb-3">
+                <label for="post-content" class="form-label">Content</label>
+                <textarea class="form-control" id="post-content" rows="10" name="content"></textarea>
+            </div>
+            <div class="text-end">
+                <button type="submit" class="btn btn-primary">Add</button>
+            </div>
+            <input type="hidden" name="csrf_token" value="<?php echo CSRF::getToken("add_post_form"); ?>" />
+        </form>
+    </div>
+    <div class="text-center">
+        <a href="/manage-posts" class="btn btn-link btn-sm"><i class="bi bi-arrow-left"></i> Back to Posts</a>
+    </div>
+</div>
+<?php
+    
+    require dirname(__DIR__) . '/parts/footer.php';
